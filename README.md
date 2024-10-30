e1348037f2e37c46bf8aab3387d96cb14e1b0eb2
Chern Jin Liew <cody.liew@ideagenplc.com>
2024-10-30 16:52:49 +0800

    first commit

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-# Spin the Wheel game
-![Cloudflare](https://img.shields.io/badge/Cloudflare-F38020?style=for-the-badge&logoColor=white&label=Powered%20By)
+# This is a food finder for Huddle Teams in Ideagen
 
-This project utilises ReactJs best practices to build a spin the wheel like game 
-![spinner](https://user-images.githubusercontent.com/77246158/206507735-f6674a7e-accb-4e29-8ce6-a9cb5bf58bd6.png)
-
-
-### Made Using-
-- Framework [ReactJS](https://reactjs.org/)
-- Confetti [Confetti](https://github.com/Agezao/confetti-js)
-- Deployed using [CloudFlare](https://pages.dev)
-
-Hope you enjoy it!!!
+Please head to this website to use the food finder. Enjoy ur meal :3
--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import "./index.css";
-import { React, useState } from "react";
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { React } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Login from "./components/Login";
 
 function App() {
- return ( 
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Login/>}/>
-    <Route path="/home" element={<Home/>}/>
-  </Routes>
-  </BrowserRouter>
-
- )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
